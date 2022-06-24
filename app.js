6f05ef6abdfcb83a127c8e5548dac001730e72b3
MSKose <saitmustafa1998@gmail.com>
2022-06-24 17:47:25 +0200

    html and js part done, styling with css needed

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const btnDiv = document.querySelector('.buttons');
 const minute = document.querySelector('.minute');
 const second = document.querySelector('.second');
 const millisecond = document.querySelector('.millisecond');
+const faSolid =document.querySelector('.fa-solid')
+console.log(faSolid);
 
 console.log(resetBtn);
 
@@ -12,23 +14,60 @@ let mls = 0
 let sec = 0
 let min = 0
 
-toggleBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    toggleBtn.innerHTML = `<i class="fa-solid fa-circle-pause">`
-    let myInterval = setInterval(timer, 10)
-})
 
-function timer () {
-    if (mls < 100) {
-        millisecond.innerHTML = mls;
-        mls++;
+function clock() {
+    mls++
+    if (mls<9) {
+        millisecond.innerHTML = `0${mls}`
+    } 
+    if (mls > 9) {
+        millisecond.innerHTML = mls
+    }  
+    if (mls > 99) {
+        sec++
+        second.innerHTML = `0${sec}`
+        mls = 0
+        millisecond.innerHTML = `0${0}`
+    }
+    if (sec > 9) {
+        second.innerHTML = sec
+    }
+    if (sec > 59) {
+        min++
+        minute.innerHTML = `0${min}`
+        sec = 0
+    }
+}
 
-        if (mls == 100) {
-            second.innerHTML = sec;
-            sec++
-            mls == 0
-            timer()
-        }
+
+toggleBtn.addEventListener('click', () => {
+    
+    if (faSolid.classList.contains('fa-circle-play')){
+        toggleBtn.innerHTML = `<i class="fa-solid fa-circle-pause">`
+        faSolid.classList.remove('fa-circle-play');
+        faSolid.classList.add('fa-circle-pause');
+        myInterval = setInterval(clock, 0.5)                             // spent almost half an hour here. So I was 
+        // initially declaring myInterval with let and was having initialization error in the else if part. Removing 
+        // the let I came around it cuz it's now global
+    }
+    else if (faSolid.classList.contains('fa-circle-pause')) {
+        toggleBtn.innerHTML = `<i class="fa-solid fa-circle-play">`
+        faSolid.classList.remove('fa-circle-pause');
+        faSolid.classList.add('fa-circle-play');
+        clearInterval(myInterval)
     }
     
-}
+})
+
+
+resetBtn.addEventListener('click', () => {
+    clearInterval(myInterval)
+    mls = 0
+    sec = 0 
+    min = 0
+    millisecond.innerHTML = `00`
+    second.innerHTML = `00`
+    minute.innerHTML = `00`
+    toggleBtn.innerHTML = `<i class="fa-solid fa-circle-play">`
+    faSolid.classList.add('fa-circle-play');
+})
